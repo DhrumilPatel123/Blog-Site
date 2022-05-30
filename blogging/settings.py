9f108eeb8d6144187551a164b56d26b8dbe59c83
DhrumilPatel123 <57946905+DhrumilPatel123@users.noreply.github.com>
2022-05-30 18:34:19 +0530

    Update settings.py

--- blogging/settings.py
+++ blogging/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Imaes)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
